839def557b8a840d9fa9a85ec5a102439545037e
Thiago Almeida <t.leandroalmeida@gmail.com>
2021-07-29 09:44:15 -0300

    MainViewController

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,46 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable {
+
+	// itens de controle de tela para corresponder ao menu da tela
+	
+	@FXML
+	private MenuItem menuItemSeller;
+	
+	@FXML
+	private MenuItem menuItemDepartment;
+	
+	@FXML
+	private MenuItem menuItemAbout;
+	
+	// eventHandlers (tratar eventos do Menu)
+	
+	@FXML
+	public void onMenuItemSellerAction() {
+		System.out.println("onMenuItemSellerAction");
+	}
+	
+	@FXML
+	public void onMenuItemDepartmentAction() {
+		System.out.println("onMenuItemDepartmentAction");
+	}
+	
+	@FXML
+	public void onMenuItemAboutAction() {
+		System.out.println("onMenuItemAboutAction");
+	}
+	
+	@Override
+	public void initialize(URL uri, ResourceBundle resourceBundle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
