dc6eb4bd9417f4cafeec0d8c9e29cabee81d96bf
Thiago Almeida <t.leandroalmeida@gmail.com>
2021-08-09 08:32:55 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,19 +1,22 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constratints;
 import gui.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
 import model.services.DepartmentService;
 
@@ -26,6 +29,9 @@ public class DepartmentFormController implements Initializable {
 	// declaração dos componentes da tela
 	
 	private DepartmentService service;
+	
+	// objetos que receberão o evento
+	private List<DataChangeListener> dataChangedListeners = new ArrayList<>();
 	
 	@FXML
 	private TextField txtId;
@@ -50,6 +56,10 @@ public class DepartmentFormController implements Initializable {
 		this.service = service;
 	}
 	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangedListeners.add(listener);
+	}
+	
 	// declaração dos métodos para tratar os botões
 	
 	public void onBtSaveAction(ActionEvent event) {
@@ -66,6 +76,10 @@ public class DepartmentFormController implements Initializable {
 			
 			// salva no banco de dados
 			service.saveOrUpdate(entity);
+			
+			// fazer a notificação
+			notifyDataChangeListeners();
+			
 			Utils.currentStage(event).close();
 		}
 		catch (DbException e) {
@@ -73,6 +87,13 @@ public class DepartmentFormController implements Initializable {
 		}
 	}
 	
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangedListeners) {
+			listener.onDataChanget();
+		}
+		
+	}
+
 	// pega os dados do formulário e retorna um novo objeto
 	private Department getFormData() {
 		Department obj = new Department();
---
+++ src/gui/listeners/DataChangeListener.java
@@ -0,0 +1,8 @@
+package gui.listeners;
+
+public interface DataChangeListener {
+
+	// evento a ser disparado quando os dados mudarem
+	void onDataChanget();
+	
+}
